582b0577f5ebef963395ddcd55d7d323513831ab
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-12-30 15:44:32 +0530

    typography

---
+++ src/pages/Create.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Typography from "@mui/material/Typography"; //import { Typography } from "@mui/material";
+
+export default function Create() {
+	return (
+		<div>
+			<Typography variant="p" component="div" align="center">
+				loremljdnggggggggggggjvlksl ljfsnblgks lfknlksf glkb
+				gldkflbknldxklkbldxkg blkx gl lf/ gblk lk loremljdnggggggggggggjvlksl
+				ljfsnblgks lfknlksf glkb gldkflbknldxklkbldxkg blkx gl lf/ gblk lk
+				loremljdnggggggggggggjvlksl ljfsnblgks lfknlksf glkb
+				gldkflbknldxklkbldxkg blkx gl lf/ gblk lk loremljdnggggggggggggjvlksl
+				ljfsnblgks lfknlksf glkb gldkflbknldxklkbldxkg blkx gl lf/ gblk lk
+				loremljdnggggggggggggjvlksl ljfsnblgks lfknlksf glkb
+				gldkflbknldxklkbldxkg blkx gl lf/ gblk lk loremljdnggggggggggggjvlksl
+				ljfsnblgks lfknlksf glkb gldkflbknldxklkbldxkg blkx gl lf/ gblk lk
+			</Typography>
+		</div>
+	);
+}
+
+//We pass props to component as varaint, component
+
+//variant -> applies the theme typography styles.
+//component -> The component used for the root node.Either a string to use a HTML element or a component.
+// align -> set the text-align on the component
+//gutterBottom -> If true, the text will have a bottom margin.
+// noWrap -> If true, the text will not wrap, but instead will truncate with a text overflow.

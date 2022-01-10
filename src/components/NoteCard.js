31746da8c2b1ec56fb9a0b29afbaba0a806bcc59
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-01-10 14:02:39 +0530

    card & delete from db

---
+++ src/components/NoteCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import { DeleteOutlined } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+
+export default function NoteCard({ note, handleDelete }) {
+	return (
+		<div>
+			<Card elevation={3}>
+				<CardHeader
+					action={
+						<IconButton onClick={() => handleDelete(note.id)}>
+							<DeleteOutlined />
+						</IconButton>
+					}
+					title={note.title}
+					subheader={note.category}
+				/>
+				<CardContent>
+					<Typography variant="body2" color="textSecondary">
+						{note.details}
+					</Typography>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
+
+// elevation property on card
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
+import NoteCard from "../components/NoteCard";
 
 export default function Notes() {
 	const [notes, setNotes] = useState([]);
@@ -12,28 +13,22 @@ export default function Notes() {
 			.then((data) => setNotes(data));
 	}, []);
 
-	return (
-		<Container maxWidth="xl">
-			{/* <Grid container>
-				<Grid item xs={12} sm={6} md={3}>
-					<Paper>1</Paper>
-				</Grid>
-				<Grid item xs={12} sm={6} md={3}>
-					<Paper>2</Paper>
-				</Grid>
-				<Grid item xs={12} sm={6} md={3}>
-					<Paper>3</Paper>
-				</Grid>
-				<Grid item xs={12} sm={6} md={3}>
-					<Paper>4</Paper>
-				</Grid>
-			</Grid> */}
+	const handleDelete = async (id) => {
+		await fetch("http://localhost:8000/notes/" + id, {
+			method: "DELETE",
+		});
 
-			<Grid container>
+		const newNotes = notes.filter((note) => note.id != id);
+		setNotes(newNotes);
+	};
+
+	return (
+		<Container>
+			<Grid container spacing={3}>
 				{notes.map((note) => {
 					return (
 						<Grid item key={note.id} xs={12} md={6} lg={4}>
-							<Paper>{note.title}</Paper>
+							<NoteCard note={note} handleDelete={handleDelete} />
 						</Grid>
 					);
 				})}
@@ -42,11 +37,4 @@ export default function Notes() {
 	);
 }
 
-// grid container -> is like wrapper around all the element inside the grid
-// grid item --> they are the thing that is going to laid on the grid
-
-// it 12 columns system
-//md -> medium size screen and up
-// xs -> extra small size
-// sm --> small size screen
-// lg --> large
+// we want to add spacing in our grid system
